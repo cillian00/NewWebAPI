bfaeb15f16d9f715ef4974231e82172ea8daba9f
cillian00 <20094972@mail.wit.ie>
2022-11-28 16:16:52 +0000

    nodelab01, simple server

--- nodelab-01/index.js
+++ nodelab-01/index.js
@@ -7,7 +7,7 @@ dotenv.config();
 const port = process.env.PORT;
 // Configure our HTTP server to respond with Hello World to all requests.
 const server = http.createServer((req, res) => {
-  res.writeHead(200);
+  req.writeHead(200);
   res.end('WAD2');
 });
 
