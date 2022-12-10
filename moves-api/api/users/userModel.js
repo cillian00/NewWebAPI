e3087d3bce730f5cdc176b06305438ccb26b86e7
cillian00 <20094972@mail.wit.ie>
2022-12-10 22:34:27 +0000

    Mongo lab Users Router

--- moves-api/api/users/userModel.js
+++ moves-api/api/users/userModel.js
@@ -2,9 +2,33 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-  username: { type: String, unique: true, required: true},
-  password: {type: String, required: true }
+const MovieSchema = new Schema({
+    id: Number,
+    title: String
+  });
+  
+  const UserSchema = new Schema({
+    username: { type: String, unique: true, required: true},
+    password: {type: String, required: true },
+    favourites: [MovieSchema]
+  });
+
+  /// ... Code as Before
+router.post('/:id/favourites', async (req, res) => {
+    const newFavourite = req.body;
+    if (newFavourite && newFavourite.id) {
+        const user = await User.findById(req.params.id);
+        if (user) {
+            user.favourites.push(newFavourite);
+            user.save();
+            res.status(201).json({ code: 201, msg: "Added Favourite" });
+        } else {
+            res.status(404).json({ code: 404, msg: 'Unable to add favourites' });
+        }
+    }
 });
+  
+
+
 
 export default mongoose.model('User', UserSchema);
