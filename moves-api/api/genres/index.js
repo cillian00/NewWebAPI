f656e2953ee955b99f5304cc120814134173b115
cillian00 <20094972@mail.wit.ie>
2022-12-10 23:24:07 +0000

    Mongo lab Exercises

--- moves-api/api/genres/index.js
+++ moves-api/api/genres/index.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { genres } from './genreData';
+import Genre from './genderModel';
 import uniqid from 'uniqid'
 
 
 const router = express.Router(); 
-router.get('/', (req, res) => {
-    res.json(genres);
+router.get('/', async (req, res) => {
+    const genres = await Genre.find();
+    res.status(200).json(genres);
 });
-
 export default router;
