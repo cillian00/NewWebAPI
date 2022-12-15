a24306769dd89304932e31e8ef223abe2e2e18cd
cillian00 <20094972@mail.wit.ie>
2022-12-15 17:00:25 +0000

    Load files

--- moves-api/api/genres/index.js
+++ moves-api/api/genres/index.js
@@ -1,11 +1,23 @@
 import express from 'express';
-import Genre from './genderModel';
+import { genres } from './genreData';
 import uniqid from 'uniqid'
 
-
 const router = express.Router(); 
-router.get('/', async (req, res) => {
-    const genres = await Genre.find();
-    res.status(200).json(genres);
+router.get('/', (req, res) => {
+    res.json(genres);
 });
+
+// Get genres details
+router.get('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (genres.id == id) {
+        res.status(200).json(genres);
+    } else {
+        res.status(404).json({
+            message: 'The resource you requested could not be found.',
+            status_code: 404
+        });
+    }
+});
+
 export default router;
